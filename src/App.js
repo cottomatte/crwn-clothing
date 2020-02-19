924df341decf9ccb28fd1849d618033b9255d8a0
Dusty <dms1905@gmail.com>
2020-02-19 15:25:47 -0500

    Finishing sign in and handlesubmit method

--- src/App.js
+++ src/App.js
@@ -25,15 +25,12 @@ class App extends React.Component {
                 const userRef = await createUserProfileDocument(userAuth)
 
                 userRef.onSnapshot(snapshot => {
-                    this.setState(
-                        {
-                            currentUser: {
-                                id: snapshot.id,
-                                ...snapshot.data()
-                            }
-                        },
-                        () => console.log(this.state.currentUser)
-                    )
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    })
                 })
             } else {
                 this.setState({ currentUser: userAuth })
